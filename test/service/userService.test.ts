1da80a2f583e4e0fa593bbd2d6274f95e4fe947c
JHON JAIME GIL SEPULVEDA <JGS808@wnutibara.com.co>
2018-10-17 13:20:20 -0500

    Circle CI

---
+++ test/service/userService.test.ts
@@ -0,0 +1,28 @@
+import { instance, mock, when, anything } from 'ts-mockito';
+import { expect } from 'chai';
+import { UserRepository } from '../../src/repository/userRepository';
+import { VehicleRepository } from '../../src/repository/vehicleRepository';
+import { UserService, UserServiceImp } from '../../src/service/userService';
+import { User } from '../../src/entity/user';
+
+describe('UserService', () => {
+    let userRepository: UserRepository;
+    let vehicleRepository: VehicleRepository;
+    let userService: UserService;
+
+    beforeAll(async done => {
+        userRepository = mock(UserRepository);
+        vehicleRepository = mock(VehicleRepository);
+        userService = new UserServiceImp(userRepository, vehicleRepository);
+        done();
+    });
+
+    describe('getAll', () => {
+        it('should get all', async () => {
+            const list = new Array<User>();
+            when(userRepository.findAll()).thenResolve(list);
+            const result = await userService.getAll();
+            expect(result).to.be.equal(list);
+        });
+    });
+});
